5a03f683b0aa52314903368c327d9fc25d82d2ab
SarvajeetLuthra <167876707+SarvajeetLuthra@users.noreply.github.com>
2025-04-16 21:31:20 +0530

    Update coco.py

--- sample2/coco/coco.py
+++ sample2/coco/coco.py
@@ -7,21 +7,6 @@ Configurations and data loading code for MS COCO.
 Usage: import the module (see Jupyter notebooks for examples), or run from
        the command line as such:
 
-    # Train a new model starting from pre-trained COCO weights
-    python3 coco.py train --dataset=/path/to/coco/ --model=coco
-
-    # Train a new model starting from ImageNet weights. Also auto download COCO dataset
-    python3 coco.py train --dataset=/path/to/coco/ --model=imagenet --download=True
-
-    # Continue training a model that you had trained earlier
-    python3 coco.py train --dataset=/path/to/coco/ --model=/path/to/weights.h5
-
-    # Continue training the last model you trained
-    python3 coco.py train --dataset=/path/to/coco/ --model=last
-
-    # Run COCO evaluatoin on the last model you trained
-    python3 coco.py evaluate --dataset=/path/to/coco/ --model=last
-"""
 
 import os
 import sys
